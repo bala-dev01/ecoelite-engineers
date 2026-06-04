a50d425e61fa3a25974ef3a7767a65ac4056b566
bala-dev01 <kbalakrishna.dev@gmail.com>
2026-06-04 18:05:17 +0530

    new updates

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O33z192VkEKGwDyVQUF2ho3eXZKTLlbJadWq72K0Km4=\"\n}"
--- dist/dev/static/chunks/src_app_page_tsx_0z-9oz5._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_WebGLScene_tsx_01-5uu_._.js","static/chunks/src_05es6~6._.js","static/chunks/node_modules_0y-kljl._.js"],
-    source: "dynamic"
-});
